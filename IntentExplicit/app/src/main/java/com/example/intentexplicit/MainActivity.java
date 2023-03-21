5aaf871b3b209e13687c82e1a1be5ac04e22a70b
ismiatioktafia <ismiatiekaokavia@gmail.com>
2023-03-21 09:54:28 +0700

    Minggu Ke 6

---
+++ IntentExplicit/app/src/main/java/com/example/intentexplicit/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.intentexplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    Button btnSend;
+
+    private String KEY_NAME = "NAMA";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.edt_nama);
+        btnSend = (Button) findViewById(R.id.btn_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String nama = name.getText().toString();
+                    if (nama != ""){
+                        Intent i = new Intent(MainActivity.this, ActivitySecond.class);
+                        i.putExtra(KEY_NAME, nama);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(getApplication(), "YOU NEED TO FILL YOUR NAME",Toast.LENGTH_LONG);
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "ERROR, TRY AGAIN !", Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
+}
